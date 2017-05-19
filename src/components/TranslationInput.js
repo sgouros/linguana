38ee52cc11a77b6fd327987eb41f6b7d784a0519
George Sgouros <sgouros@sgouros.gr>
2017-05-19 10:07:52 +0300

    special key double press fix

--- src/components/TranslationInput.js
+++ src/components/TranslationInput.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 class TranslationInput extends Component {
   s_Timeout = 0;
+  alreadyPressedSpecialKeyCode = -1;
 
   special_letter_substitutions = [
     [83, "s", "ß"],
@@ -14,10 +15,6 @@ class TranslationInput extends Component {
     this.props.onChange(event);
   };
 
-  specialKeyAlreadyPressed = () => {
-    return this.s_Timeout === 0 ? false : true;
-  };
-
   handleKeyDown = event => {
     if (
       event.keyCode === 65 ||
@@ -25,40 +22,50 @@ class TranslationInput extends Component {
       event.keyCode === 79 ||
       event.keyCode === 83
     ) {
-      this.handleSpecialKeyPress(event);
+      if (this.sameSpecialKeyPressed(event.keyCode)) {
+        this.handleSameSpecialKeyPress(event);
+      } else {
+        this.handleSpecialKeyPress(event);
+      }
     } else if (event.keyCode === 27) {
       this.handleEscPress(event);
     } else {
-      console.debug("normal key pressed");
+      console.info("normal key pressed");
     }
   };
 
-  tick = () => {
-    console.debug(`auto clearing special key press timeout ${this.s_Timeout}`);
+  resetSpecialKeyPress = () => {
+    console.info(`Clearing special key press timeout ${this.s_Timeout}`);
+    clearTimeout(this.s_Timeout);
     this.s_Timeout = 0;
+    this.alreadyPressedSpecialKeyCode = -1;
   };
 
   handleSpecialKeyPress = event => {
-    const index_of_substitution_pair = this.special_letter_substitutions.findIndex(
+    this.resetSpecialKeyPress(); // get rid of previous presses
+    this.s_Timeout = setTimeout(this.resetSpecialKeyPress, 190);
+    console.info(`Special key (${event.keyCode}) pressed for the FIRST time. Setting timeout ${this.s_Timeout}`);
+    this.alreadyPressedSpecialKeyCode = event.keyCode;
+  };
+
+  handleSameSpecialKeyPress = event => {
+    console.info("SAME special key pressed AGAIN");
+
+    const initial_value = event.target.value;
+    const specialKeyPairIndex = this.special_letter_substitutions.findIndex(
       item => item[0] === event.keyCode
     );
+    const correct_input_box_value =
+      initial_value.substr(0, initial_value.length - 1) +
+      this.special_letter_substitutions[specialKeyPairIndex][2];
+    event.target.value = correct_input_box_value;
 
-    if (this.specialKeyAlreadyPressed()) {
-      console.debug("special key already pressed.");
-      clearTimeout(this.s_Timeout);
-      this.s_Timeout = 0;
-      const initial_value = event.target.value;
-      const correct_input_box_value =
-        initial_value.substr(0, initial_value.length - 1) +
-        this.special_letter_substitutions[index_of_substitution_pair][2];
-      event.target.value = correct_input_box_value;
-      event.preventDefault();
-    } else {
-      this.s_Timeout = setTimeout(this.tick, 190);
-      console.debug(
-        `special key NOT already pressed. Setting timeout ${this.s_Timeout}`
-      );
-    }
+    this.resetSpecialKeyPress();
+    event.preventDefault();
+  };
+
+  sameSpecialKeyPressed = keyCode => {
+    return keyCode === this.alreadyPressedSpecialKeyCode;
   };
 
   handleEscPress = event => {

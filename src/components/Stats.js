844c62f1dd42fd181bd85774fe542f457b6a50eb
George Sgouros <sgouros@sgouros.gr>
2017-06-29 08:40:26 +0300

    αλλαγή εικονιδίων check redcross στο footer με yes no

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -13,11 +13,7 @@ class Stats extends Component {
       <div className="stats__component">
         <div>
           <div className="stats__component__totalCorrectAnswers">
-            <img
-              className="stats__component__footerImg"
-              src="/img/greenCheck.png"
-              alt="correct answers"
-            />
+            <img className="stats__component__footerImg" src="/img/yes.png" alt="correct answers" />
             <div className="stats__component__circledNumber stats__component--greenCircle">
               {this.props.correctTranslationsCount}
             </div>
@@ -25,7 +21,7 @@ class Stats extends Component {
         </div>
         <div>
           <div className="stats__component__totalEntries">
-            <img className="stats__component__footerImg" src="/img/total.png" alt="total entries" />
+            {/*<img className="stats__component__footerImg" src="/img/total.png" alt="total entries" />*/}
             <div className="stats__component__circledNumber stats__component--blueCircle">
               {this.props.totalEntriesCount}
             </div>
@@ -33,10 +29,10 @@ class Stats extends Component {
         </div>
         <div>
           <div className="stats__component__totalWrongAnswers">
-            <img className="stats__component__footerImg" src="/img/redCross.png" alt="wrong answers" />
             <div className="stats__component__circledNumber stats__component--redCircle">
               {this.props.wrongTranslationsCount}
             </div>
+            <img className="stats__component__footerImg" src="/img/no.png" alt="wrong answers" />
           </div>
         </div>
       </div>

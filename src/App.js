27f6c673b245316b6131dbdb59448653724af3ff
George Sgouros <sgouros@sgouros.gr>
2017-10-03 11:40:30 +0300

    by George Sgouros

--- src/App.js
+++ src/App.js
@@ -14,11 +14,8 @@ import { getDateString, getShortDateString } from "./components/helpers.js";
 import DebugButtons from "./components/DebugButtons.js";
 import SearchForm from "./components/SearchForm.js";
 import HeaderLogo from "./components/HeaderLogo.js";
-import VocabularyEntry from "./components/VocabularyEntry.js";
 
 // todo:
-//      * να μπορείς να κάνεις edit
-//      * όταν περνάς παρατήρηση σε λέξη, να μπορείς να έχεις οποιαδήποτε γλώσσα εκεί
 //      * οταν πατάς enter στις αρχικές λέξεις που δείχνει να γίνεται dismiss το modal
 //      * κάθε φορά που ανανεώνεται η βάση των stats να γίνεται κάτι trigger
 //        και αυτά να απεικονίζονται στην αρχική οθόνη

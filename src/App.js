10fa643c6f70185c133c99020b8965e93beb219b
George Sgouros <sgouros@sgouros.gr>
2017-10-31 09:30:07 +0200

    -

--- src/App.js
+++ src/App.js
@@ -601,7 +601,7 @@ export default class App extends Component {
     } else {
       return (
         <div className="app" tabIndex="0" onKeyDown={this.toggleCssSkin}>
-          {/* <link rel="stylesheet" type="text/css" href={this.state.cssSkin} /> */}
+          <link rel="stylesheet" type="text/css" href={this.state.cssSkin} />
           <header className="app__header">
             <HeaderLogo ifClicked={this.goToStartPage} />
             <DebugButtons

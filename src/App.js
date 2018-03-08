6a4370ff28904c5878ef53d8a9b21bed223730de
George Sgouros <sgouros@sgouros.gr>
2018-03-08 12:46:32 +0200

    by George Sgouros

--- src/App.js
+++ src/App.js
@@ -47,8 +47,8 @@ export default class App extends Component {
     this.totalWordsLearnedForTodayArray = [];
     this.submittedEntriesFromVocabularyManager = [];
   }
-  NUMBER_OF_NEW_VOC_ENTRIES = 1;
-  NUMBER_OF_OLD_VOC_ENTRIES = 1;
+  NUMBER_OF_NEW_VOC_ENTRIES = 3;
+  NUMBER_OF_OLD_VOC_ENTRIES = 7;
   
   passKeyAlreadyPressed = false;
   passKeyTimeout = 0;

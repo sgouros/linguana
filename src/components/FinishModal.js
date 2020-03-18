05831d68e665f419f5bd265e69e38fca92906d7d
George Sgouros <sgouros@sgouros.gr>
2020-03-18 14:06:35 +0200

    finish modal lifting

--- src/components/FinishModal.js
+++ src/components/FinishModal.js
@@ -13,11 +13,14 @@ export default class FinishModal extends Component {
     return (
       <ModalContainer onClose={this.props.onClose} id="finishModal">
         <ModalDialog onClose={this.props.onClose} id="finishModal" dismissOnBackgroundClick={true} width="70%">
-          <h1 className="finishModalTitle">{this.props.title}</h1>
           <div className="finishModalImages">
             <img className="finishModalLinguanaFaceImg" src="/img/correct.png" alt="happy linguana" />
             <img className="css-congratulations-img" src="/img/congratulations.jpg" alt="congratulations" />
           </div>
+          <h1 className="finishModalTitle">{this.props.title}</h1>
+          <button autoFocus className="customModal__okButton" onClick={this.props.onClose}>
+            Hurray!!
+          </button>
         </ModalDialog>
       </ModalContainer>
     );
